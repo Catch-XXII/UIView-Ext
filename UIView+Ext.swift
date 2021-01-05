409c3724c2d725a51ff454ab69b3274b87205765
Catch-XXII <cgultekink@gmail.com>
2021-01-05 10:18:41 +0300

    Create UIView+Ext.swift
    extension to UIview for adding constraint with formatted string

---
+++ UIView+Ext.swift
@@ -0,0 +1,11 @@
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String:UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
